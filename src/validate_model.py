44da5774d198c26d98a9956037f4ce3f90b60ed6
Sinhué Siordia Millán <carpediem.sinhue@gmail.com>
2026-06-11 16:52:49 -0700

    V1.0.2 (#8) pipeline github actions
    * Ajustes de pipeline.yml
    
    * ajuste de branch para githubaction
    
    * ajustes de codigo por flake8
    
    * archivos requeridos para pipeline
    
    * ajustes de importacion config c
    
    * pruebas con make all
    
    * ajustes de flake8
    
    * ajustes de pipeline.yml para mlflow
    
    * ajustes en pipeline.yml
    
    * ajustes de compose.yml
    
    * ajustes de ruta relativa mlflow en compose.yml
    
    * ajustes de compose por path de log mlflow

--- src/validate_model.py
+++ src/validate_model.py
@@ -5,41 +5,42 @@ Si falla, el pipeline CI/CD se detiene y no se construye la imagen Docker.
 """
 import sys
 import logging
-import json
 
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 import config as C
+import json
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s | VALIDATE_MODEL | %(levelname)s | %(message)s',
                     datefmt='%H:%M:%S')
 
 log = logging.getLogger(__name__)
 
-# ── Funciones para gestionar metricas json ────────────────────────────────────── 
+# ── Funciones para gestionar metricas json ──────────────────────────────────────
 
-def obtener_metricas_json(metrics_path: Path = C.METRICS_PATH) -> tuple[dict,dict,dict]:
-    metricas_dict=dict()
-    
+
+def obtener_metricas_json(metrics_path: Path = C.METRICS_PATH) -> tuple[dict, dict, dict]:
+    metricas_dict = dict()
+
     with open(metrics_path, "r", encoding="utf-8") as f:
         metricas_dict = json.load(f)
-    
-    log.info(f'📊 ARCHIVO DE MÉTRICAS DETECTADO - RESUMEN DE RESULTADOS:\n{json.dumps(metricas_dict, indent=4, ensure_ascii=False)}')    
+
+    log.info(f'📊 ARCHIVO DE MÉTRICAS DETECTADO - RESUMEN DE RESULTADOS:\n{json.dumps(metricas_dict, indent=4, ensure_ascii=False)}')
     algoritmo_modelo = {"algoritmo": metricas_dict["algoritmo"]}
     hiperparametros_modelo = metricas_dict["hiperparametros_finales"]
     resultados_modelo = metricas_dict["metricas_evaluacion"]
-    
+
     log.info(f"🔹 Modelo: {algoritmo_modelo}")
     log.info(f"🔹 Hiperparámetros: {hiperparametros_modelo}")
     log.info(f"🔹 Métricas: {resultados_modelo}")
-    
-    return algoritmo_modelo,hiperparametros_modelo,resultados_modelo
+
+    return algoritmo_modelo, hiperparametros_modelo, resultados_modelo
 
 
-def quality_gate(algoritmo:dict,hiperparametros:dict,metricas:dict) -> None:
-    
+def quality_gate(algoritmo: dict, hiperparametros: dict, metricas: dict) -> None:
+
     umbral = C.UMBRAL_MIN
-    algoritmo=algoritmo["algoritmo"]
+    algoritmo = algoritmo["algoritmo"]
     accuracy = metricas["accuracy"]
     f1_score = metricas["f1_score"]
     recall = metricas["recall"]
@@ -58,26 +59,28 @@ def quality_gate(algoritmo:dict,hiperparametros:dict,metricas:dict) -> None:
         print(f"\n FALLO: Recall {recall:.4f} < umbral {umbral}")
         print(" El pipeline CI/CD se detiene..")
         sys.exit(1)
-    
+
     print("=" * 50)
     print(f"\n APROBADO: Recall {recall:.4f} >= {umbral}")
-    print(" Pipeline CI/CD continúa.")    
+    print(" Pipeline CI/CD continúa.")
     print("=" * 50)
 
-# ── Función principal ───────────────────────────────────────────────────── 
+# ── Función principal ─────────────────────────────────────────────────────
 
-def run():    
+
+def run():
     log.info('=== ETAPA 5: VALIDACION DE RESULTADOS DEL MODELO ===')
-     
-    if not C.METRICS_PATH.exists():        
-        raise FileNotFoundError(f'Ruta del metricas no encontradas: {C.METRICS_PATH}')    
-        sys.exit(1)  
 
-    algoritmo,hiperparametros,metricas=obtener_metricas_json()
-    quality_gate(algoritmo,hiperparametros,metricas)
-    
-    log.info('=== ETAPA 5 COMPLETADA ===')  
+    if not C.METRICS_PATH.exists():
+        raise FileNotFoundError(f'Ruta del metricas no encontradas: {C.METRICS_PATH}')
+        sys.exit(1)
+
+    algoritmo, hiperparametros, metricas = obtener_metricas_json()
+    quality_gate(algoritmo, hiperparametros, metricas)
+
+    log.info('=== ETAPA 5 COMPLETADA ===')
     return None
 
-if __name__ == '__main__':    
+
+if __name__ == '__main__':
     run()
